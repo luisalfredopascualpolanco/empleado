97817f42c0f3e6c122ee0382c9e16d4e8be2fd78
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2022-01-06 12:21:33 -0800

    version1.0

---
+++ proyecto13.5/proyecto13.5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto13._5
+{
+    class Program
+    {
+        public class Empleados
+        {
+            private string nombre;
+            private float sueldo;
+
+            public void Cargar()
+            {
+                Console.Write("Nombre del empleado: ");
+                nombre = Console.ReadLine();
+
+                Console.Write("Sueldo del empleado: ");
+                sueldo = float.Parse(Console.ReadLine());
+            }
+
+            public void Imprimir()
+            {
+                Console.WriteLine("Nombre: "+nombre);
+                Console.WriteLine("Sueldo: "+sueldo);
+            }
+
+            public void Impuesto() 
+            {
+                if (sueldo > 3000)
+                {
+                    Console.WriteLine("Usted tiene que pagar impuestos");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Empleados empleado = new Empleados();
+                empleado.Cargar();
+                empleado.Imprimir();
+                empleado.Impuesto();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ha introducido un dato invalido.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
